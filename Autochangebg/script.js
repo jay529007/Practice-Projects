a6732933562f6f0a738f8c6f1c158fa23094e16d
jay529007 <jaysatapara.3@gmail.com>
2025-03-27 18:49:37 +0530

    new project
    generate auto color

---
+++ Autochangebg/script.js
@@ -0,0 +1,47 @@
+//generate a random color
+
+// ------------------------ chai aur code logic for genrate randome color ------------------------
+// const randomColor = function () {
+//     const hex = '0123456789ABCDEF';
+//     let color = '#';
+//     for (let i = 0; i < 6; i++) {
+//       color += hex[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+//   };
+
+// ------------------------ chatGPT logic for genrate randome color ------------------------
+const randomColor = () => {
+  const randomColor =
+    "#" +
+    Math.floor(Math.random() * 0xffffff)
+      .toString(16)
+      .padStart(6, "0");
+  return randomColor;
+};
+// console.log(randomColor);
+
+const bdcolor = document.body.style.backgroundColor;
+// console.log(bdcolor);
+
+let intervalId;
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+  document.body.style.backgroundColor = bdcolor;
+  console.log(bdcolor);
+  
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
